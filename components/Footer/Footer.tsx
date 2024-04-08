4964565b1fb3b6d07dd6778192a6cc6f1d67f2ec
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-08 22:59:17 +0200

    update css

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,27 +1,26 @@
-import Image from 'next/image'
-import React from 'react'
-import { Button } from '../ui/button'
-import { Input } from '../ui/input'
+import Image from "next/image";
 import logoBlack from "../../public/images/logoBlack.svg";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 
 const Footer = () => {
   return (
-    <div className="m-8">
-        <div className="flex flex-col items-center lg:mt-20">
-          <Image src={logoBlack} alt="logo" className="" />
-          <span className="uppercase lg:m-12 lg:text-[20px] mt-8">
-            What&apos; s News ?{" "}
-          </span>
-          <div className="flex flex-col md:flex-row md:gap-x-4">
-            <Input
-              placeholder="Email Adress"
-              className="w-40 lg:w-80 mt-4 lg:text-xl lg:p-8"
-            />
-            <Button className="w-40 mt-4 lg:text-xl lg:p-8">SIGN UP</Button>
-          </div>
+    <div className="bg-slate-100 p-12">
+      <div className="flex flex-col items-center lg:mt-20">
+        <Image src={logoBlack} alt="logo" className="" />
+        <span className="mt-8 uppercase lg:m-12 lg:text-[20px]">
+          What&apos; s News ?{" "}
+        </span>
+        <div className="flex flex-col md:flex-row md:gap-x-4">
+          <Input
+            placeholder="Email Adress"
+            className="mt-4 w-40 lg:w-80 lg:p-8 lg:text-xl"
+          />
+          <Button className="mt-4 w-40 lg:p-8 lg:text-xl">SIGN UP</Button>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
