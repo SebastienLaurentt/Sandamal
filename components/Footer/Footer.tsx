63b54c8e8985d240f36c9b587725a01d998ae40a
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-08 23:06:11 +0200

    use now SandamalIcon in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import logoBlack from "../../public/images/logoBlack.svg";
+import SandamalIcon from "../SandamalIcon/SandamalIcon";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 
@@ -7,7 +6,7 @@ const Footer = () => {
   return (
     <div className="bg-slate-100 p-12">
       <div className="flex flex-col items-center lg:mt-20">
-        <Image src={logoBlack} alt="logo" className="" />
+        <SandamalIcon fillColor="black" />
         <span className="mt-8 uppercase lg:m-12 lg:text-[20px]">
           What&apos; s News ?{" "}
         </span>
