1c5af6bd78171365f0cd7b6e11f3866f1cde523b
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-08 13:13:58 +0200

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import imgHome from "../public/images/HomePage2.png";
+import logo from "../public/images/logo.png";
 
 export default function Home() {
   return (
-<div className="">
-  <Image src={imgHome} alt="HomePageImg" className="bg-cover bg-center w-full" />
-</div>
+    <div className="mb-20">
+      <Image
+        src={imgHome}
+        alt="HomePageImg"
+        className="bg-cover bg-center w-full"
+      />
+      <div className="flex flex-col items-center mt-8 lg:mt-20">
+        <Image src={logo} alt="logo" className="" />
+        <span className="uppercase lg:m-12 lg:text-[20px]">What&apos; s News ? </span>
+        <div className="flex flex-col md:flex-row md:gap-x-4">
+          <Input placeholder="Email Adress" className="w-40 lg:w-80 mt-4 lg:text-xl lg:p-8" />
+          <Button className="w-40 mt-4 lg:text-xl lg:p-8">SIGN UP</Button>
+        </div>
+      </div>
+    </div>
   );
 }
--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
 
   return (
     <header className="absolute py-4">
-      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
+      <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl ">
         <Link href="/">
           <Image src={logo} alt="Logo Sandamal" />
         </Link>
