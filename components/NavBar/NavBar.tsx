67899fedfa63891f10a1432619d331176af82008
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-08 12:47:27 +0200

    update navBar css

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
   const pathname = usePathname();
 
   return (
-    <header className="mb-8 border-b">
+    <header className="py-4">
       <div className="flex items-center justify-between mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl">
         <Link href="/">
           <Image src={logo} alt="Logo Sandamal" />
@@ -43,7 +43,7 @@ export default function Navbar() {
           ))}
         </nav>
 
-        <div className="flex divide-x border-r sm:border-l">
+        {/* <div className="flex divide-x border-r sm:border-l">
           <Button
             variant={"outline"}
             className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 sm:w-20 md:h-24 md:w-24 rounded-none"
@@ -53,7 +53,7 @@ export default function Navbar() {
               Cart
             </span>
           </Button>
-        </div>
+        </div> */}
 
         <div className="h-8 md:h-10 flex items-center lg:hidden ">
           <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
