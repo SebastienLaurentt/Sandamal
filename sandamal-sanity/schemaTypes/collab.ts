5e5b165569ddbbb1375674121c7725f92a1a4857
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-09 23:14:09 +0200

    create collab schema

---
+++ sandamal-sanity/schemaTypes/collab.ts
@@ -0,0 +1,12 @@
+export default {
+  name: 'collab',
+  type: 'document',
+  title: 'Collab',
+  fields: [
+    {
+      name: 'collabImage',
+      title: 'Image',
+      type: 'image',
+    },
+  ],
+}
