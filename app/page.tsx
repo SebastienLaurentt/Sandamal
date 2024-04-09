01e246c62e292176095ffe7e924a6258b723fff7
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-09 18:05:22 +0200

    create hero variant with video (responsive)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import SectionBg from "@/components/Section/SectionBg/SectionBg";
-import imgHome from "../public/images/HomePage2.png";
-
 export default function Home() {
   return (
     <main>
-      <SectionBg bg={imgHome} minHeightScreen={true}></SectionBg>
+      {/* <SectionBg bg={imgHome} minHeightScreen={true}></SectionBg> */}
+      <video
+        src={require("../public/videos/Red.mp4")}
+        autoPlay
+        muted
+        loop
+        className="size-full object-cover object-center"
+        style={{ minHeight: "100vh" }} // Pour assurer que la vidéo couvre au moins toute la hauteur de l'écran
+      ></video>
     </main>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+import withVideos from "next-videos";
 const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
 };
 
-export default nextConfig;
+export default {
+  ...nextConfig,
+  ...withVideos(),
+};
