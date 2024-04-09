e252a9a54ceb739e04024af9ae00bbcffd8179ec
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-09 23:17:02 +0200

    add collab, draw and sketch in schemaTypes

--- sandamal-sanity/schemaTypes/index.ts
+++ sandamal-sanity/schemaTypes/index.ts
@@ -1,3 +1,6 @@
+import collab from "./collab";
+import draw from "./draw";
 import paint from "./paint";
+import sketch from "./sketch";
 
-export const schemaTypes = [paint]
+export const schemaTypes = [paint, collab, draw, sketch]
