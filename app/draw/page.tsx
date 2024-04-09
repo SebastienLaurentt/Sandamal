c54d386b15d57250c8318959bfe819c53a97e0da
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-09 23:33:17 +0200

    add DrawFetch, backToTop and Footer

--- app/draw/page.tsx
+++ app/draw/page.tsx
@@ -1,10 +1,16 @@
+import BackToTop from "@/components/BackToTop/BackToTop";
+import DrawFetch from "@/components/DataFetch/DrawFetch/DrawFetch";
+import Footer from "@/components/Footer/Footer";
 import Section from "@/components/Section/Section";
 
 const Draw = () => {
   return (
     <main>
       <Section>
+        <DrawFetch />
       </Section>
+      <BackToTop />
+      <Footer />
     </main>
   );
 };
