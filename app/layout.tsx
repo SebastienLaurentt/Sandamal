ab67339d3e352a5c04c7dc4965d8480f39d88587
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-09 23:06:00 +0200

    remove comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
-        {/* <BackToTop /> */}
-        {/* <Footer /> */}
       </body>
     </html>
   );
