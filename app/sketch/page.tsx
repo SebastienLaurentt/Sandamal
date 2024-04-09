0af83bd80cec765d33853b3b4f74f204bfd3eb53
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-09 23:36:33 +0200

    add sketchFetch, backToTop and Footer

--- app/sketch/page.tsx
+++ app/sketch/page.tsx
@@ -1,11 +1,16 @@
-import SignUp from "@/components/Footer/Footer";
+import BackToTop from "@/components/BackToTop/BackToTop";
+import SketchFetch from "@/components/DataFetch/SketchFetch/SketchFetch";
+import Footer from "@/components/Footer/Footer";
 import Section from "@/components/Section/Section";
 
 const Sketch = () => {
   return (
     <main>
       <Section>
+        <SketchFetch />
       </Section>
+      <BackToTop />
+      <Footer />
     </main>
   );
 };
