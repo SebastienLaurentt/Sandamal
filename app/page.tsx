e022c288e23eb97c9d87ee064b5fe314d0effebb
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-10 11:17:57 +0200

    display video with next.js reco

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,28 @@ export default function Home() {
       {/* <SectionBg bg={imgHome} minHeightScreen={true}></SectionBg> */}
       <Footer />
       <video
+        preload="yes"
+        autoPlay
+        loop
+        className="absolute -z-50 size-full object-cover"
+      >
+        <source src={require("../public/videos/Lilith.mp4")} type="video/mp4" />
+        <track
+          src={require("../public/videos/Lilith.mp4")}
+          kind="subtitles"
+          srcLang="en"
+          label="English"
+        />
+        Your browser does not support the video tag.
+      </video>
+      {/* <video
         src={require("../public/videos/Lilith.mp4")}
         autoPlay
         muted
         loop
         className="absolute -z-50 size-full object-cover"
-        style={{ minHeight: "100vh" }} 
-      ></video>
+        style={{ minHeight: "100vh" }}
+      ></video> */}
     </main>
   );
 }
