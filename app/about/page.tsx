7dc4682ee5dbacfdc77b8f4cf37764ecd2b38c53
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-10 16:01:24 +0200

    add footer and backToTop

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import BackToTop from "@/components/BackToTop/BackToTop";
+import Footer from "@/components/Footer/Footer";
 import Section from "@/components/Section/Section";
 import aboutImg from "@/public/images/about.jpg";
 import Image from "next/image";
@@ -6,13 +8,15 @@ const About = () => {
   return (
     <main>
       <Section>
-        <div className="flex flex-col xl:flex-row xl:gap-x-4 xl:p-4 ">
+        <div className="my-8 flex flex-col xl:flex-row xl:gap-x-4 xl:p-4 ">
           <Image src={aboutImg} alt="About" />
           <div>
             <h2>About Me</h2>
             <p>Ohhh le essseuuuuh</p>
           </div>
         </div>
+        <BackToTop />
+        <Footer />
       </Section>
     </main>
   );
