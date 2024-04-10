3cf3cd979947535a5bc5ae0d613c1a0a90658f5c
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-10 17:30:33 +0200

    update content and css

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,20 +12,34 @@ const About = () => {
           <Image
             src={aboutImg}
             alt="About"
-            className="xl:w-1/2 xl:p-16 2xl:w-2/5"
+            className="xl:w-1/2 xl:p-16 2xl:px-32"
           />
           <div>
-            <h2>About Me</h2>
-            <p>Ohhh le essseuuuuh</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio
-              laborum quod labore iusto praesentium aperiam totam repellendus
-              nesciunt, expedita consequatur hic ut debitis illum ex beatae!
-              Nemo, quos. Laboriosam saepe delectus voluptas, sapiente,
-              accusamus expedita reiciendis harum, reprehenderit corporis
-              aperiam optio laudantium! Voluptas voluptatem, dignissimos earum
-              dolore quis similique reiciendis?
-            </p>
+            <h2>Quick Bio</h2>
+            <div className="flex flex-col gap-y-2 xl:max-w-[600px]">
+              <p>My name is Julien Sandamal Perez </p>
+              <p>
+                I prefer Sandamal. it is my born name and it mean Moon Flower.
+              </p>
+              <p>
+                I was born in Sri Lanka, grew up and raised in France, and
+                currently live in Lyon in France. I studied graphic design, 3D
+                animation. I work in that field as Genaralist 3D during about 9
+                years.
+              </p>
+              <p>
+                I&apos;m currently working on my own projects (sketch, paint,
+                draw, music as well).
+              </p>
+              <p>
+                It is time for me to show what I&apos;ve done and so I decide to
+                create this website to show you my univers
+              </p>
+              <p>
+                E-Mail me if you want to say Hello:
+                perez.julien.sandamal@gmail.com
+              </p>
+            </div>
           </div>
         </div>
         <BackToTop />
