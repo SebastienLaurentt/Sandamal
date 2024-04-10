15a023cca704daca08aeeed5600d00026871d2d3
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-10 16:13:32 +0200

    update content and css

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,11 +8,24 @@ const About = () => {
   return (
     <main>
       <Section>
-        <div className="my-8 flex flex-col xl:flex-row xl:gap-x-4 xl:p-4 ">
-          <Image src={aboutImg} alt="About" />
+        <div className="my-8 flex flex-col items-center xl:flex-row xl:gap-x-4 xl:p-8 ">
+          <Image
+            src={aboutImg}
+            alt="About"
+            className="xl:w-1/2 xl:p-16 2xl:w-2/5"
+          />
           <div>
             <h2>About Me</h2>
             <p>Ohhh le essseuuuuh</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio
+              laborum quod labore iusto praesentium aperiam totam repellendus
+              nesciunt, expedita consequatur hic ut debitis illum ex beatae!
+              Nemo, quos. Laboriosam saepe delectus voluptas, sapiente,
+              accusamus expedita reiciendis harum, reprehenderit corporis
+              aperiam optio laudantium! Voluptas voluptatem, dignissimos earum
+              dolore quis similique reiciendis?
+            </p>
           </div>
         </div>
         <BackToTop />
