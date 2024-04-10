c0f96bb5bdeadd6f22eea2b2e723a4b02c37759e
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-10 11:39:40 +0200

    update video import

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Footer from "@/components/Footer/Footer";
+import Video from "../public/videos/Lilith.mp4";  
+
 
 export default function Home() {
   return (
@@ -11,7 +13,7 @@ export default function Home() {
         loop
         className="absolute -z-50 size-full object-cover"
       >
-        <source src={require("../public/videos/Lilith.mp4")} type="video/mp4" />
+        <source src={Video} type="video/mp4" />
       </video>
       {/* <video
         src={require("../public/videos/Lilith.mp4")}
