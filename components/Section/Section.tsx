071e702931964d148225f80a21c6c8f227e0b782
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-11 23:55:08 +0200

    add marginTop and marginBottom props in section component

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
 interface SectionProps {
+  marginTop?: boolean;
+  marginBottom?: boolean;
   classname?: string;
   children: React.ReactNode;
 }
 
-const Section = ({ classname, children }: SectionProps) => {
+const Section = ({ marginTop, marginBottom, classname, children }: SectionProps) => {
+  const isMarginTop = marginTop ? "" : "";
+  const isMarginBottom = marginBottom ? "" : "";
   return (
-    <section className={` ${classname} mt-10 px-4 md:px-8 lg:px-12 xl:px-16`}>
+    <section className={` ${classname} ${isMarginTop} ${isMarginTop} mt-10 px-4 md:px-8 lg:px-12 xl:px-16`}>
       {children}
     </section>
   );
