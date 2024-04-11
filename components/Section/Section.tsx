b49080bf1483eff205e5bf23fd3ce6b8be4084aa
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-11 23:55:50 +0200

    update css

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -8,8 +8,8 @@ interface SectionProps {
 }
 
 const Section = ({ marginTop, marginBottom, classname, children }: SectionProps) => {
-  const isMarginTop = marginTop ? "" : "";
-  const isMarginBottom = marginBottom ? "" : "";
+  const isMarginTop = marginTop ? "mt-8 md:mt-12 lg:mt-16" : "";
+  const isMarginBottom = marginBottom ? "mb-8 md:mb-12 lg:mb-16" : "";
   return (
     <section className={` ${classname} ${isMarginTop} ${isMarginTop} mt-10 px-4 md:px-8 lg:px-12 xl:px-16`}>
       {children}
