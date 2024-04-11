2826922715f66c86095b03f509858fde86887574
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-11 16:32:02 +0200

    update with getData

--- app/collab/[slug]/page.tsx
+++ app/collab/[slug]/page.tsx
@@ -1,9 +1,25 @@
+import { client } from '@/app/lib/sanity';
 import React from 'react'
 
-const CollabPage = () => {
+
+async function getData(slug: string) {
+  const query = `*[_type == "collab" && slug.current == "${slug}"][0] {
+        _id,
+          images,
+          name,
+          "slug": slug.current,
+      }`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export const dynamic = "force-dynamic";
+
+export default async function CollabPage () {
   return (
     <div>page</div>
   )
 }
 
-export default CollabPage
