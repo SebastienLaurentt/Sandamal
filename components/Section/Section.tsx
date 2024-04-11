320cd2204077aa83512528ca273f1f5ce88ad5dc
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-11 23:51:35 +0200

    add classname props in section component

--- components/Section/Section.tsx
+++ components/Section/Section.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 interface SectionProps {
+  classname?: string;
   children: React.ReactNode;
 }
 
-const Section = ({ children }: SectionProps) => {
+const Section = ({ classname, children }: SectionProps) => {
   return (
-    <section className="mt-10 px-4 md:px-8 lg:px-12 xl:px-16">
+    <section className={` ${classname} mt-10 px-4 md:px-8 lg:px-12 xl:px-16`}>
       {children}
     </section>
   );
