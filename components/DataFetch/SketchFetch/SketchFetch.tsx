73f87569a6936cd4613a39617e366dc051ad4629
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-11 23:50:30 +0200

    update css

--- components/DataFetch/SketchFetch/SketchFetch.tsx
+++ components/DataFetch/SketchFetch/SketchFetch.tsx
@@ -12,7 +12,7 @@ async function getData() {
 export default async function SketchFetch() {
   const data = await getData();
   return (
-    <div className="flex flex-col gap-y-4 md:grid md:grid-cols-2 md:gap-2 md:px-4 lg:grid-cols-3 lg:gap-4 lg:px-6  xl:px-8">
+    <div className="flex flex-col gap-y-4 md:grid md:grid-cols-2 md:gap-2 md:px-4 lg:grid-cols-3 xl:grid-cols-5 lg:gap-4 lg:px-6  xl:px-8">
       {data.map((sketch: any, index: number) => (
         <div key={index} className=" ">
           <div className="rounded-md   ">
