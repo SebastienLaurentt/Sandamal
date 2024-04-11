29d08b9d35cc2b97e0bbe65cd8d1817bb91ba886
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-12 00:18:04 +0200

    update screen display

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,9 @@ const config = {
       center: true,
       padding: "2rem",
       screens: {
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
         "2xl": "1400px",
       },
     },
