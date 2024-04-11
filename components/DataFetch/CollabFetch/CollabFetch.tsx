1c7a317c2ed5c00fa1f6436e40d1e773b8156990
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-11 16:23:33 +0200

    update collabFetch with simplifiedCollab interface

--- components/DataFetch/CollabFetch/CollabFetch.tsx
+++ components/DataFetch/CollabFetch/CollabFetch.tsx
@@ -1,9 +1,15 @@
+import { simplifiedCollab } from "@/app/interface";
 import { client, urlFor } from "@/app/lib/sanity";
 import Image from "next/image";
 import Link from "next/link";
 
 async function getData() {
-  const query = `*[_type == "collab"]`;
+  const query = `*[_type == "collab"] {
+    _id,
+    name,
+    "slug": slug.current,
+    "imageUrl": collabMainImage.asset->url
+  }`;
 
   const data = await client.fetch(query);
 
@@ -11,7 +17,7 @@ async function getData() {
 }
 
 export default async function CollabFetch() {
-  const data = await getData();
+  const data: simplifiedCollab[] = await getData();
   return (
     <div className="flex flex-col gap-y-4 md:grid md:grid-cols-2 md:gap-2 md:px-4 lg:grid-cols-3 lg:gap-4 lg:px-6  xl:px-8">
       {data.map((collab: any, index: number) => (
@@ -19,7 +25,7 @@ export default async function CollabFetch() {
           <Link href={`/collab/${collab.slug}`}>
             <div className="rounded-md">
               <Image
-                src={urlFor(collab.collabMainImage).url()}
+                src={collab.imageUrl}
                 alt="Great Photo"
                 className="size-full object-cover object-center"
                 priority
