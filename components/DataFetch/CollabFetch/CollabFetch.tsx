c5d8371a48f434fe1fe1385302c09390042c03f0
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-11 16:15:08 +0200

    nest collabImage with slugLink

--- components/DataFetch/CollabFetch/CollabFetch.tsx
+++ components/DataFetch/CollabFetch/CollabFetch.tsx
@@ -1,5 +1,6 @@
 import { client, urlFor } from "@/app/lib/sanity";
 import Image from "next/image";
+import Link from "next/link";
 
 async function getData() {
   const query = `*[_type == "collab"]`;
@@ -15,16 +16,18 @@ export default async function CollabFetch() {
     <div className="flex flex-col gap-y-4 md:grid md:grid-cols-2 md:gap-2 md:px-4 lg:grid-cols-3 lg:gap-4 lg:px-6  xl:px-8">
       {data.map((collab: any, index: number) => (
         <div key={index} className=" ">
-          <div className="rounded-md   ">
-            <Image
-              src={urlFor(collab.collabMainImage).url()}
-              alt="Great Photo"
-              className="size-full object-cover object-center"
-              priority
-              width={500}
-              height={500}
-            />
-          </div>
+          <Link href={`/collab/${collab.slug}`}>
+            <div className="rounded-md">
+              <Image
+                src={urlFor(collab.collabMainImage).url()}
+                alt="Great Photo"
+                className="size-full object-cover object-center"
+                priority
+                width={500}
+                height={500}
+              />
+            </div>
+          </Link>
         </div>
       ))}
     </div>
