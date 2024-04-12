a225aea8e87512e95a3ab5142fe793674a1af5bb
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-12 12:14:33 +0200

    update css

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -9,36 +9,38 @@ const Footer = () => {
 
   const textColor =
     pathname === "/" ? "text-secondary-foreground" : "text-foreground";
-  const placeHolderTextButton =
+  const emailButtonBorderColor =
+    pathname === "/" ? "border-white" : "border-black";
+  const emailPlaceholderTextColor =
     pathname === "/"
       ? "placeholder:text-secondary-foreground"
       : "placeholder:text-foreground";
   const followMeTextColor =
     pathname === "/" ? "text-foreground" : "text-secondary-foreground";
-  const followMeButtonColor = pathname === "/" ? "bg-background" : "bg-primary";
-  const emailButtonBorderColor =
-    pathname === "/" ? "border-white" : "border-black";
+  const followMeBgColor = pathname === "/" ? "bg-background" : "bg-primary";
+  const followMeHover =
+    pathname === "/" ? "hover:bg-transparent hover:text-white" : "hover:bg-background hover:text-foreground hover:border-2 hover:border-black";
 
   return (
     <div className="  pb-20 xl:pb-12">
       <div className="flex flex-col items-center">
-        <span className={` text-[20px] uppercase ${textColor} lg:text-[40px]`}>
+        <span className={` text-md uppercase ${textColor} lg:text-3xl`}>
           Keep in touch
         </span>
         <div className="flex flex-col md:flex-row md:gap-x-2 ">
           <Input
             placeholder="Email"
-            className={`mx-auto mt-4 w-40 border-2 ${emailButtonBorderColor} ${textColor} ${placeHolderTextButton} bg-transparent`}
+            className={`mx-auto mt-4 w-40 border-2 ${emailButtonBorderColor} ${textColor} ${emailPlaceholderTextColor} bg-transparent`}
           />
           <Button
-            className={`mt-4 w-40 ${followMeButtonColor} ${followMeTextColor}`}
+            className={`mt-4 w-40 ${followMeBgColor} ${followMeTextColor} ${followMeHover}`}
           >
             FOLLOW ME
           </Button>
         </div>
         <div className="flex flex-row pt-4">
           <p
-            className={`${textColor} max-w-[200px] text-center text-[14px] leading-5 xl:max-w-[300px] xl:text-[20px] xl:leading-6`}
+            className={`${textColor} max-w-[200px] text-center text-[14px] leading-5 xl:max-w-[300px] xl:text-md xl:leading-6`}
           >
             By the way, my name is Sandamal. Welcome to my lands.
           </p>
