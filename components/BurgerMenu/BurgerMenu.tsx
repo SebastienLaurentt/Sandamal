834999e0c8eb83636aab84e54e8e7c06d1436f8c
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-12 12:58:34 +0200

    use justify-between in burgerMenu

--- components/BurgerMenu/BurgerMenu.tsx
+++ components/BurgerMenu/BurgerMenu.tsx
@@ -35,33 +35,35 @@ export default function BurgerMenu({ isOpen, setIsOpen }: IBurgerMenu) {
       </button>
 
       <div
-        className={`fixed left-0 top-0  flex min-h-screen w-full flex-col items-center bg-white pt-12 text-center text-black transition-all duration-500 ease-in-out  ${
+        className={`fixed left-0 top-0  flex min-h-screen w-full flex-col justify-between bg-white py-12 text-center text-black transition-all duration-500 ease-in-out  ${
           isOpen ? "opacity-100" : "hidden opacity-0"
         }`}
       >
-        <Link href="/">
-          <SandamalIcon fillColor="black" />
-        </Link>
+        <div className="flex flex-col items-center justify-center">
+          <Link href="/">
+            <SandamalIcon fillColor="black" />
+          </Link>
 
-        <span className="my-3 text-3xl lg:text-6xl xl:hidden xl:text-xl">
-          <span className="mr-4 lg:mr-6">S</span>
-          <span className="mr-4 lg:mr-6">A</span>
-          <span className="mr-4 lg:mr-6">N</span>
-          <span className="mr-4 lg:mr-6">D</span>
-          <span className="mr-4 lg:mr-6">A</span>
-          <span className="mr-4 lg:mr-6">M</span>
-          <span className="mr-4 lg:mr-6">A</span>
-          <span>L</span>
-        </span>
+          <span className="my-3 text-3xl lg:text-6xl xl:hidden xl:text-xl">
+            <span className="mr-4 lg:mr-6">S</span>
+            <span className="mr-4 lg:mr-6">A</span>
+            <span className="mr-4 lg:mr-6">N</span>
+            <span className="mr-4 lg:mr-6">D</span>
+            <span className="mr-4 lg:mr-6">A</span>
+            <span className="mr-4 lg:mr-6">M</span>
+            <span className="mr-4 lg:mr-6">A</span>
+            <span>L</span>
+          </span>
 
-        <button
-          onClick={() => setIsOpen(false)}
-          aria-label="Fermer le menu mobile"
-        >
-          <X className="lg:size-10" />
-        </button>
+          <button
+            onClick={() => setIsOpen(false)}
+            aria-label="Fermer le menu mobile"
+          >
+            <X className="lg:size-10" />
+          </button>
+        </div>
 
-        <ul className="mt-16 flex flex-col gap-6 p-4 text-md font-extralight tracking-[.5em] md:text-lg lg:mt-20 lg:gap-8 lg:text-xl">
+        <ul className=" flex flex-col gap-4 p-4 text-md font-extralight tracking-[.5em] md:text-lg lg:gap-8 lg:text-xl">
           {navData.map((link) => (
             <li key={link.name}>
               <Link href={link.href} onClick={() => setIsOpen(false)}>
@@ -72,7 +74,7 @@ export default function BurgerMenu({ isOpen, setIsOpen }: IBurgerMenu) {
         </ul>
 
         <button
-          className="mt-16 flex w-full justify-center lg:mt-20"
+          className=" flex w-full justify-center"
           onClick={() => setIsOpen(false)}
           aria-label="Allez à la page Instagram de Sandamal"
         >
