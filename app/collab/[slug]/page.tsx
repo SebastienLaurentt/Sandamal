8692839754de4f4d8839d5ff3d64a0ee2deb6908
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-15 19:33:13 +0200

    update css

--- app/collab/[slug]/page.tsx
+++ app/collab/[slug]/page.tsx
@@ -1,9 +1,8 @@
 import { simplifiedCollab } from "@/app/interface";
 import { client } from "@/app/lib/sanity";
-import Section from "@/components/Section/Section";
 import Image from "next/image";
-import LogoSihinW from "../../../public/images/LogoSihinW.jpg";
 import Logo from "../../../public/images/Logo.jpg";
+import LogoSihinW from "../../../public/images/LogoSihinW.jpg";
 import VynilUnfolded from "../../../public/images/VynilUnfolded.jpg";
 import Int01 from "../../../public/images/int01.jpg";
 import Int02 from "../../../public/images/int02.jpg";
@@ -35,30 +34,33 @@ export default async function CollabPage({
   const data: simplifiedCollab = await getData(params.slug);
   return (
     <>
-      <Section classname="sihin text-center">
-        <h2 className=" text-white">{data.name}</h2>
-        <p className="text-md text-sihinText">
-          I had the privilege of crafting the artistic ensemble for the vinyl
-          cover of the metal band Sihin. From creating captivating illustrations
-          to designing a striking logo, and meticulously orchestrating the
-          layout, every aspect of this project was an exciting exploration of
-          Sihin&apos;s dark and powerful aesthetic. Explore a glimpse of my work
-          for this unique collaboration.
-        </p>
-        <div className=" flex flex-row gap-x-14 py-8">
-          <Image src={VynilFront} alt="Sihin" className="w-1/3" />
-          <Image src={Vynil} alt="Sihin" className="w-2/3" />
-        </div>
+      <div className="sihin text-center">
         <div>
+          <h2 className=" text-white">{data.name}</h2>
+          <p className="text-md">
+            I had the privilege of crafting the artistic ensemble for the vinyl
+            cover of the metal band Sihin. From creating captivating
+            illustrations to designing a striking logo, and meticulously
+            orchestrating the layout, every aspect of this project was an
+            exciting exploration of Sihin&apos;s dark and powerful aesthetic.
+            Explore a glimpse of my work for this unique collaboration.
+          </p>
+        </div>
+
+        <div className=" flex w-fit flex-row gap-x-14 p-8">
+          <Image src={VynilFront} alt="Sihin" className="" />
+          <Image src={Vynil} alt="Sihin" className="" />
+        </div>
+        <div className="px-0">
           <Image src={VynilUnfolded} alt="Sihin" />
         </div>
-        <div className="flex w-full flex-row  gap-x-8 py-8">
+        <div className="mx-auto flex w-fit flex-row justify-center gap-x-8 p-8">
           <Image src={Int01} alt="Sihin" />
           <Image src={Int02} alt="Sihin" />
           <Image src={Int03} alt="Sihin" />
           <Image src={Int04} alt="Sihin" />
         </div>
-      </Section>
+      </div>
       <div className="flex flex-row justify-center bg-white py-16">
         <Image src={LogoSihinW} alt="Sihin" className="size-96" />
       </div>
