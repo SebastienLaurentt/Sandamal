145519cede90d292071e5776b1cb17dbfe219675
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-17 15:54:33 +0200

    install resend and create action.ts

---
+++ app/src/actions.ts
@@ -0,0 +1,40 @@
+"use server";
+import { Resend } from "resend";
+
+interface State {
+  error: string | null
+  success: boolean
+}
+
+// Fonction asynchrone qui renvoie la valeur de runtime
+export async function getRuntime(): Promise<string> {
+  return 'edge';
+}
+
+// Fonction asynchrone qui renvoie la valeur de dynamic
+export async function getDynamic(): Promise<string> {
+  return 'force-dynamic';
+}
+
+export async function sendEmail(prevState: State, formData: FormData) {
+  const email = formData.get("email") as string
+  try {
+    const resend = new Resend(process.env.RESEND_API_KEY)
+    await resend.emails.send({
+      from: 'Sandamal Website <onboarding@resend.dev>',
+      to: 'slaurent.dev@gmail.com',
+      subject: `Nouvelle inscription depuis ${email}`,
+      text:  `Nouvelle inscription depuis ${email}`
+    })
+    return {
+      error: null,
+      success: true
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      error: (error as Error).message,
+      success: false
+    }
+  }
+}
