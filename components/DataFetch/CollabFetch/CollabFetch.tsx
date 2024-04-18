84ab30f244094e016da9e03fb512368678307921
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-18 16:35:21 +0200

    display larger img from md display

--- components/DataFetch/CollabFetch/CollabFetch.tsx
+++ components/DataFetch/CollabFetch/CollabFetch.tsx
@@ -9,7 +9,8 @@ async function getData() {
     name,
     shortDescription,
     "slug": slug.current,
-    "imageUrl": collabMainImage.asset->url
+    "imageUrlMobile": mainImageMobile.asset->url,
+    "imageUrlTab": mainImageTab.asset->url
   }`;
 
   const data = await client.fetch(query);
@@ -20,20 +21,28 @@ async function getData() {
 export default async function CollabFetch() {
   const data: simplifiedCollab[] = await getData();
   return (
-    <div className="flex flex-col md:grid md:grid-cols-2 xl:grid-cols-3">
+    <div className="flex flex-col ">
       {data.map((collab: any, index: number) => (
         <div key={index}>
           <Link href={`/collab/${collab.slug}`}>
             <div className="relative -z-10 ">
               <Image
-                src={collab.imageUrl}
+                src={collab.imageUrlMobile}
                 alt="Great Photo"
-                className="size-full object-cover object-center"
+                className=" size-full object-cover object-center md:hidden"
                 priority
                 width={500}
                 height={500}
               />
-              <div className="absolute bottom-8 left-8 flex w-fit flex-col gap-y-2 uppercase text-white lg:bottom-12 lg:left-12 ">
+              <Image
+                src={collab.imageUrlTab}
+                alt="Great Photo"
+                className="hidden size-full object-cover object-center md:block"
+                priority
+                width={500}
+                height={500}
+              />
+              <div className="absolute bottom-8 left-8 flex size-full flex-col items-center justify-center gap-y-2 uppercase text-white md:left-0 md:top-0 ">
                 <span className="text-3xl font-semibold leading-8">
                   {collab.name}
                 </span>
