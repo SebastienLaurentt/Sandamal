d6f24c9e458a922944e1e99978593fdb60ccb4a9
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-18 11:38:51 +0200

    update favicon import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
+import BackToTop from "@/components/BackToTop/BackToTop";
+import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/NavBar/NavBar";
 import type { Metadata } from "next";
 import { Dosis } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/Footer/Footer";
-import BackToTop from "@/components/BackToTop/BackToTop";
 
 const inter = Dosis({ subsets: ["latin"] });
 
@@ -18,7 +18,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr" suppressHydrationWarning>
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={inter.className}>
         <Navbar />
         {children}
