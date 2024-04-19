6c4c8935143f45f7fc70d064c13d9900d9f25b90
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-19 02:15:25 +0200

    card hover add black opacity on img and text become whiter

--- components/DataFetch/CollabFetch/CollabFetch.tsx
+++ components/DataFetch/CollabFetch/CollabFetch.tsx
@@ -20,16 +20,20 @@ async function getData() {
 
 export default async function CollabFetch() {
   const data: simplifiedCollab[] = await getData();
+
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col">
       {data.map((collab: any, index: number) => (
-        <div key={index}>
-          <Link href={`/collab/${collab.slug}`} className="text-white">
-            <div className="relative -z-10 ">
+        <div key={index} className="group relative">
+          <Link
+            href={`/collab/${collab.slug}`}
+            className=" text-white md:text-white/50 md:group-hover:text-white"
+          >
+            <div className="relative -z-10">
               <Image
                 src={collab.imageUrlMobile}
                 alt="Great Photo"
-                className=" size-full object-cover object-center md:hidden "
+                className="size-full object-cover object-center md:hidden"
                 priority
                 width={500}
                 height={500}
@@ -37,12 +41,12 @@ export default async function CollabFetch() {
               <Image
                 src={collab.imageUrlTab}
                 alt="Great Photo"
-                className="hidden size-full object-cover object-center  md:block"
+                className="hidden size-full object-cover object-center md:block"
                 priority
                 width={500}
                 height={500}
               />
-              <div className="absolute bottom-8 left-8 flex size-full flex-col items-start justify-end gap-y-2 md:left-0 md:top-0 md:flex-row md:items-center md:justify-center md:gap-x-2 ">
+              <div className="absolute bottom-8 left-8 z-10 flex size-full flex-col items-start justify-end gap-y-2 md:left-0 md:top-0 md:flex-row md:items-center md:justify-center md:gap-x-2">
                 <span className="text-3xl font-semibold uppercase leading-8">
                   {collab.name}
                 </span>
@@ -50,6 +54,7 @@ export default async function CollabFetch() {
                   {collab.shortDescription}
                 </span>
               </div>
+              <div className="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 md:group-hover:opacity-50"></div>
             </div>
           </Link>
         </div>
