bfb486ce550d83ea578f7285afd362b8d3f9122c
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-19 15:12:14 +0200

    update content and css

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,37 +6,38 @@ const About = () => {
   return (
     <main>
       <Section paddingHorizontal={true}>
-        <div className="my-8 flex flex-col items-center xl:flex-row xl:gap-x-4 xl:p-8 ">
+        <div className="my-8 flex flex-col items-center lg:flex-row lg:gap-x-4 lg:p-2 xl:p-8 ">
           <Image
             src={aboutImg}
             alt="About"
-            className="xl:w-1/2 xl:p-16 2xl:px-32"
+            className="lg:w-1/2 lg:p-2 xl:p-16 2xl:px-32"
           />
           <div>
             <h2>Who am i ?</h2>
             <div className="flex flex-col gap-y-2 xl:max-w-[600px]">
-              <p>My name is Julien Sandamal Perez </p>
               <p>
-                I prefer Sandamal. it is my born name and it mean Moon Flower.
+                Verily, I am Sandamal Perez, yet I beseech thee to address me as
+                Sandamal, a name bestowed upon me akin to the &quot;Moon
+                Flower&quot;, a symbol of my origins.
               </p>
               <p>
-                I was born in Sri Lanka, grew up and raised in France, and
-                currently live in Lyon in France. I studied graphic design, 3D
-                animation. I work in that field as Genaralist 3D during about 9
-                years.
+                Born in the lands of Sri Lanka, I was reared amidst the fields
+                of France, Lyon now being my humble dwelling. In the noble arts
+                of graphic design and 3D animation, I have spent nigh on nine
+                years as a Generalist 3D artisan.
               </p>
               <p>
-                I&apos;m currently working on my own projects (sketch, paint,
-                draw, music as well).
+                At this hour, I labor upon mine own ventures, comprising the
+                crafts of sketching, painting, drawing, and the harmonious
+                strains of musical composition.
               </p>
               <p>
-                It is time for me to show what I&apos;ve done and so I decide to
-                create this website to show you my univers
+                Tis now the appointed time for me to unveil my works. Thus, I
+                embark upon this website, a digital scroll wherein I shall
+                reveal unto thee my realm of creativity.
               </p>
-              <p>
-                E-Mail me if you want to say Hello:
-                perez.julien.sandamal@gmail.com
-              </p>
+              <p>With warm regards,</p>
+              <p>Sandamal</p>
             </div>
           </div>
         </div>
