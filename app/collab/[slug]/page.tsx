c6d7987c57e74c4598e736a92704beb75f57ad98
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-19 15:37:55 +0200

    update sihin page with new img

--- app/collab/[slug]/page.tsx
+++ app/collab/[slug]/page.tsx
@@ -1,15 +1,15 @@
 import { simplifiedCollab } from "@/app/interface";
 import { client } from "@/app/lib/sanity";
 import Image from "next/image";
-import LogoSihinW from "../../../public/images/LogoSihinW.jpg";
+import SihinCastle from "../../../public/images/Sihin_Castle.jpg";
+import SihinColumn from "../../../public/images/Sihin_Column.jpg";
+import SihinVisage from "../../../public/images/Sihin_Visage.jpg";
+import SihinWriting from "../../../public/images/Sihin_Writing.jpg";
 import VynilUnfolded from "../../../public/images/VynilUnfolded.jpg";
-import Int01 from "../../../public/images/int01.jpg";
-import Int02 from "../../../public/images/int02.jpg";
-import Int03 from "../../../public/images/int03.jpg";
-import Int04 from "../../../public/images/int04.jpg";
-import Logo from "../../../public/images/logo.jpg";
 import Vynil from "../../../public/images/vynil.png";
 import VynilFront from "../../../public/images/vynil_front.jpg";
+import LogoSihinW from "../../../public/images/LogoSihinW.jpg";
+import Logo from "../../../public/images/Logo.jpg";
 
 async function getData(slug: string) {
   const query = `*[_type == "collab" && slug.current == "${slug}"][0] {
@@ -34,7 +34,7 @@ export default async function CollabPage({
   const data: simplifiedCollab = await getData(params.slug);
   return (
     <>
-      <div className="sihin pb-20 pt-60 text-center lg:pt-80  xl:pt-40">
+      <div className="sihin pt-60 text-center lg:pt-80  xl:pt-40">
         <div className="">
           <h2 className=" text-white">{data.name}</h2>
           <p className="mx-auto px-8 pb-4 pt-8 text-md lg:max-w-[900px] xl:max-w-[1100px] ">
@@ -50,14 +50,30 @@ export default async function CollabPage({
           <Image src={VynilFront} alt="Sihin" />
           <Image src={Vynil} alt="Sihin" />
         </div>
-        <div className="px-0 py-16 lg:py-24">
+        <div className="px-0 pt-16 lg:pt-24">
           <Image src={VynilUnfolded} alt="Sihin" className="2xl:w-full" />
         </div>
-        <div className="mx-auto flex w-fit flex-col justify-center gap-8 px-8 py-16 md:flex-row md:gap-y-0 lg:py-24">
+        {/* <div className="mx-auto flex w-fit flex-col justify-center gap-8 px-8 py-16 md:flex-row md:gap-y-0 lg:py-24">
           <Image src={Int01} alt="Sihin" />
           <Image src={Int02} alt="Sihin" />
           <Image src={Int03} alt="Sihin" />
           <Image src={Int04} alt="Sihin" />
+        </div> */}
+      </div>
+      <div>
+      <div className="">
+          <Image src={SihinColumn} alt="Sihin" className="2xl:w-full" />
+        </div>
+        <div className="">
+          <Image src={SihinVisage} alt="Sihin" className="2xl:w-full" />
+        </div>
+        <div className="flex flex-row">
+          <div className="">
+            <Image src={SihinCastle} alt="Sihin" className="2xl:w-full" />
+          </div>
+          <div className="">
+            <Image src={SihinWriting} alt="Sihin" className="2xl:w-full" />
+          </div>
         </div>
       </div>
       <div className="flex flex-row justify-center bg-white py-16">
