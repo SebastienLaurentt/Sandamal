e53f95cde487003b7ccd12a28c13ec93f3297679
SebastienLaurentt <slaurent.26@gmail.com>
2024-04-24 15:20:35 +0200

    remove video in home bg (set img)

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,18 @@ import imgHome from "@/public/images/HomePage2.png";
 
 export default function Home() {
   return (
-    <main className="flex h-screen items-end justify-center ">
-      {/* <SectionBg bg={imgHome}/> */}
-      <video
+    <main className="flex  items-end justify-center ">
+      <SectionBg bg={imgHome}/>
+      {/* <video
         preload="yes"
         autoPlay
+        loop
         muted
         className="absolute -z-50 size-full object-cover"
         style={{ minHeight: "100vh" }}
       >
-        <source src="../videos/test.mp4" type="video/mp4" />
-      </video>
+        <source src="../videos/Lilith.mp4" type="video/mp4" />
+      </video> */}
     </main>
   );
 }
